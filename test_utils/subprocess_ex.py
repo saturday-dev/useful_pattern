7b073bd6015fa037dc687fcd92ed26381202a11a
saturday-dev <ineedthesleep@gmail.com>
2024-11-29 05:15:33 +0900

    Logger, getTimer 구현

--- test_utils/subprocess_ex.py
+++ test_utils/subprocess_ex.py
@@ -12,10 +12,11 @@ from utils import common_utils as cu
 
 if __name__ == '__main__':
     # subprocess로 실행하는 것을 asyncio를 이용하여 결과 기다리는 것을 순차적이 아닌 끝난 순서대로 받을 수 있는 방식
+    max_process = asyncio.Semaphore(value=300)
     cmd_list = ['ll','conda env list', 'conda list']
     task_list = []
     for cmd in cmd_list:
-        task_list.append(cu.async_do_task_and_report(cmd=cmd))
+        task_list.append(cu.async_do_task_and_report(cmd=cmd, max_process=max_process))
     loop = asyncio.get_event_loop()
     commands = asyncio.gather(*task_list)
     result = loop.run_until_complete(commands)
--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,10 +1,28 @@
 #!/home/komodo/miniconda3/envs/py311/bin/python3
 #-*- coding: utf-8 -*-
+import os
+import sys
 import asyncio
 import importlib
 from typing import Any, Union
 import logging
 from logging import Logger
+from time import time
+
+def getTimer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time()
+        result = func(*args, **kwargs) # 함수 실행
+        end_time = time()
+        elapsed_time = end_time - start_time
+        if 'logger' in kwargs.keys() and type(kwargs['logger']) == Logger:
+            logger = kwargs['logger']
+            logger.info(f'함수 {func.__name__} 소요시간은 {elapsed_time:.2f} S 입니다.')
+        else:
+            print(f'함수 {func.__name__} 소요시간은 {elapsed_time:.2f} S 입니다.')                
+        return result
+    return wrapper
+
 async def async_subprocess(cmd: str):
     proc = await asyncio.create_subprocess_shell(cmd=cmd, stderr=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE)
     stdout, stderr = await proc.communicate()
@@ -30,9 +48,18 @@ def load_class_func(modulePath, className: str):
     loaded_class = getattr(module, className)
     return loaded_class
 
-def getLogger(logPath: str, logerName: str) -> Logger :
+def getLogger(logPath: str ='./', loggerName: str = 'MyLogger') -> Logger :
+    """_summary_
+        Logger 생성하여 반환하는 함수 설정
+    Args:
+        logPath (str): _description_
+        loggerName (str): _description_
+
+    Returns:
+        Logger: _description_
+    """
     # Logger 설정
-    logger = logging.getLogger(logerName)  # 로거 이름 지정
+    logger = logging.getLogger(loggerName)  # 로거 이름 지정
     logger.setLevel(logging.DEBUG)           # 로거 레벨 설정 (INFO, WARNING 포함)
 
     # 로그 포맷 설정
@@ -46,6 +73,19 @@ def getLogger(logPath: str, logerName: str) -> Logger :
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
+    
+    # 파일 핸들러 생성
+    if os.path.isfile(logPath):
+        pass
+    elif os.path.isdir(logPath):
+        logPath = os.path.join(logPath, loggerName)
+    file_handler = logging.FileHandler(logPath)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    logger.info(f'Logger Setting Complete - {logPath=}')
+    return logger
+
+
 
 def read_file(filePath: str, logger: Logger, lazy_load: bool =False):
     with open(file=filePath) as file_to_read:
